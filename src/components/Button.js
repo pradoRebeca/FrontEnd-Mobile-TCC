59f0ceead71e5cb292c64f7988fe19e2b50acc7a
Your Name <rebeca.2003.prado@gmail.com>
2022-04-24 19:50:10 -0300

    o push funfa

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,34 +3,39 @@ import { StyleSheet, TouchableOpacity, View, Text } from "react-native";
 
 const Button = (props) => {
     return (
-        <TouchableOpacity 
+        <TouchableOpacity
             accessible={true}
-            accessibilityHint='Ir para a tela de Cadastro'
-            accessibilityLabel="Entrar" style={{...style.view, backgroundColor: props.backgroundColor }} onPress={() => props.navigation.navigate({name: props.screen})}>
+            accessibilityHint="Ir para a tela de Cadastro"
+            accessibilityLabel="Entrar"
+            style={{ ...style.view, backgroundColor: props.backgroundColor }}
+            onPress={() => props.navigation.navigate({ name: props.screen })}
+        >
             <View>
-                <Text style={{...style.text, color: props.color}}>{props.title}</Text>
+                <Text style={{ ...style.text, color: props.color }}>
+                    {props.title}
+                </Text>
             </View>
-        </TouchableOpacity >
+        </TouchableOpacity>
     );
-}
-
+};
+//teesste
 const style = StyleSheet.create({
     view: {
         height: 50,
         width: 300,
-        backgroundColor: '#225E77',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
+        backgroundColor: "#225E77",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
         borderRadius: 5,
         marginBottom: 20,
     },
     text: {
-        color: 'white',
+        color: "white",
         fontSize: 24,
         letterSpacing: 2,
-        fontWeight: '500',
+        fontWeight: "500",
     },
-})
+});
 
 export default Button;
